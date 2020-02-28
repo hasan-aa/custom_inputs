8776169f3277edec1089bf50f0fba226f35c989d
Hasan Ali Ayar <hasan.a.ayar@gmail.com>
2020-02-28 17:29:49 +0300

    initial generated gem

---
+++ lib/custom_inputs.rb
@@ -0,0 +1,6 @@
+require "custom_inputs/version"
+
+module CustomInputs
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/custom_inputs/version.rb
@@ -0,0 +1,3 @@
+module CustomInputs
+  VERSION = "0.1.0"
+end
